f7b3e63eab9f2e11cd36826c8f695a893909b33b
Sophie Popow <sophie.popow@gmail.com>
2020-04-26 01:45:34 +0200

    Lines struct implementation

--- jakSzybkoDojade/main.c
+++ jakSzybkoDojade/main.c
@@ -13,6 +13,29 @@
 #include "queue.h"
 
 
+/// MARK: - Lines implementation
+
+typedef struct Connection {
+    int travelTime;
+    int destination;
+} Connection;
+
+typedef struct Line {
+    int lineNumber;
+    int lineLength;
+    int startingStation;
+    Connection* connections;
+} Line;
+
+Line* newLine(int lineNumber, int lineLength, int startingStation) {
+    Line* l = (Line *) malloc(sizeof(Line));
+    l->lineNumber = lineNumber;
+    l->lineLength = lineLength;
+    l->startingStation = startingStation;
+    l->connections = (Connection *) malloc(lineLength*sizeof(Connection));
+    return l;
+}
+
 /// MARK: - optimalTimeMatrix - TO DO
 
 int** optimalTimeMatrix(int n){
