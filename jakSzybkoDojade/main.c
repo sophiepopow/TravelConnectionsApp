6c6dc48ed9fe6e93a901732810bd951f54a2dcc7
Sophie Popow <sophie.popow@gmail.com>
2020-04-26 03:30:45 +0200

    implementation of optimalTimeMatrix and main func

--- jakSzybkoDojade/main.c
+++ jakSzybkoDojade/main.c
@@ -36,23 +36,99 @@ Line* newLine(int lineNumber, int lineLength, int startingStation) {
     return l;
 }
 
-/// MARK: - optimalTimeMatrix - TO DO
+/// MARK: - optimalTimeMatrix
 
-int** optimalTimeMatrix(int n){
-    int** connectionsMatrix = malloc(n*sizeof(int*));
+int minTime(int a, int b){
+    if(a < b)
+        return a;
+    return b;
+}
+
+int** optimalTimeMatrix(int numberOfStations, int numberOfLines, Line** lines){
+    int** connectionsMatrix = malloc(numberOfStations*sizeof(int*));
+    for (int i = 0; i < numberOfStations; i++) {
+      connectionsMatrix[i] = malloc(numberOfStations*sizeof(int));
+    }
+
+    for (int i = 0; i < numberOfStations; i++) {
+        for (int j = 0; j < numberOfStations; j++) {
+            if (i == j)
+                connectionsMatrix[i][j] = 0;
+            else
+                connectionsMatrix[i][j] = __INT_MAX__;
+        }
+    }
     
-    
-    // TO DO
+    for (int i = 0; i < numberOfLines; i++) {
+        Line* line = lines[i];
+        int station = line->startingStation;
+        
+        for (int j = 0; j < line->lineLength; j++) {
+            
+            connectionsMatrix[station][line->connections[j].destination] = minTime(line->connections[j].travelTime, connectionsMatrix[station][line->connections[j].destination]);
+            
+            connectionsMatrix[line->connections[j].destination][station] = minTime(line->connections[j].travelTime, connectionsMatrix[line->connections[j].destination][station]);
+            
+            station = line->connections->destination;
+        }
+    }
+
+    for(int m = 0; m < numberOfStations; m++)
+        for(int i = 0; i < numberOfStations; i++)
+            for(int j = 0; j < numberOfStations; j++)
+                if(connectionsMatrix[i][j] > connectionsMatrix[i][m] + connectionsMatrix[m][j])
+                    connectionsMatrix[i][j] = connectionsMatrix[i][m] + connectionsMatrix[m][j];
     
     return connectionsMatrix;
 }
 
 int main(int argc, const char * argv[]) {
     
+/// MARK: - Lines initialization
+    
     int stations, lines, queries;
     scanf("%d %d %d",&stations, &lines, &queries);
     
+    Line** subwayLines = (Line *) malloc(lines*sizeof(Line));
+    
+    int lineLength, lineStart;
+    int travelTime, nextStation;
+    for (int i = 0; i < lines; i++) {
+        scanf("%d %d", &lineLength, &lineStart);
+        
+        // lineLength - 1 beacuse we don't count starting station
+        subwayLines[i] = newLine(i, lineLength-1, lineStart);
+        
+        for (int j = 0; j < lineLength-1; j++) {
+            scanf("%d %d", &travelTime, &nextStation);
+            subwayLines[i]->connections[j].travelTime = travelTime;
+            subwayLines[i]->connections[j].destination = nextStation;
+        }
+    }
 
+    int** shortestTravelTime = optimalTimeMatrix(stations, lines, subwayLines);
+    
+/// MARK: - Queries
+    
+    int startingPoint, finalPoint;
+    for (int i = 0; i < queries; i++) {
+        scanf("%d %d", &startingPoint, &finalPoint);
+        printf("%d", shortestTravelTime[startingPoint][finalPoint]);
+    }
+
+ /// MARK: - Free memory
+    
+    for(int i = 0; i<lines; i++) {
+        free(subwayLines[i]->connections);
+        free(subwayLines[i]);
+        
+    }
+    free(subwayLines);
+    
+    for(int i = 0; i<stations; i++) {
+        free(shortestTravelTime[i]);
+    }
+    free(shortestTravelTime);
     
     return 0;
 }
